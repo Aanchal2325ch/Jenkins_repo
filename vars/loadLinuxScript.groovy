23630a7724ab0a9d70770868bbb6cbddc8f5f442
Aanchal2325ch <81634944+Aanchal2325ch@users.noreply.github.com>
2021-08-06 18:24:06 +0530

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,7 @@
 def call(Map config =[:]){
   def scriptcontents = libraryResource 'scripts/Linux/hello-world.pipeline'
   writeFile file="${config.name}", text: scriptcontents
+  println "chmod a+x ./${config.name}"
+
   
 }
