68d0b9833cb00b5232dc23ad76b3e8ff1d7f623a
Aanchal2325ch <81634944+Aanchal2325ch@users.noreply.github.com>
2021-08-06 17:38:49 +0530

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,4 +1,4 @@
-def call{Map config =[:]}{
+def call(Map config =[:]){
   def scriptcontents = libraryResource "scripts/Linux/${config.name}"
   writeFile file="${config.name}", text: scriptcontents
   
