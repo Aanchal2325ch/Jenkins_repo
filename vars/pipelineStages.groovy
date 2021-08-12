6386e47e1ba0206e2657261325267676d3e80a27
Aanchal2325ch <81634944+Aanchal2325ch@users.noreply.github.com>
2021-08-12 12:43:22 +0530

    Update pipelineStages.groovy

--- vars/pipelineStages.groovy
+++ vars/pipelineStages.groovy
@@ -1,5 +1,12 @@
 def initStage(name)
 {
 	pipeLineUtil.validate(name)
+	def property=readProperties file:'vars/my.properties'
+	property.each
+	{
+		line-> println line
+	}
+	println property
+	println property['var1']
 	println "successful"
 }
