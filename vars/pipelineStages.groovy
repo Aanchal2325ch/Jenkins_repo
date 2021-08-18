d682b1efc7fbd7f70aaaa6822513b67e5e9dfecb
Aanchal2325ch <81634944+Aanchal2325ch@users.noreply.github.com>
2021-08-18 14:33:36 +0530

    Update pipelineStages.groovy

--- vars/pipelineStages.groovy
+++ vars/pipelineStages.groovy
@@ -28,9 +28,8 @@ def readPropertyFile(p){
 	def datas = readYaml file: 'vars/property/jenkins.yml1'
 	println datas
 	
-	p.scanpathMap.put('scan_path','hi')
         p.scan_path = p.scanpathMap.containsKey(pipeLineConstants.scan_path)
-	println p.scan_path
+	println p.scan__path
 	
 	
 	//data = readYaml file: "vars/property/jenkins.yml"
