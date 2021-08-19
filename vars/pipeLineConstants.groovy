3e0b443728fc71b51169c667b42814bc130ae02b
Aanchal2325ch <81634944+Aanchal2325ch@users.noreply.github.com>
2021-08-19 14:14:53 +0530

    Update pipeLineConstants.groovy

--- vars/pipeLineConstants.groovy
+++ vars/pipeLineConstants.groovy
@@ -2,3 +2,4 @@ import groovy.transform.Field
 @Field final scan_path = "scan_path"
 @Field final ENVIRONMENT = "environment"
 @Field final project_name="project_name"
+@Field final YML_FILE_NAME ="jenkins.yml1"
