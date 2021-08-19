b23285e8dea630d55cb9bde2ed0551f14dfe29fd
Aanchal2325ch <81634944+Aanchal2325ch@users.noreply.github.com>
2021-08-19 09:19:24 +0530

    Update pipelineStages.groovy

--- vars/pipelineStages.groovy
+++ vars/pipelineStages.groovy
@@ -31,9 +31,9 @@ def readPropertyFile(p){
 	
 	//p.scanpathMap.put(application.toString().trim,'Test')
 	//println p.scanpathMap
-	
-       // p.scan_path = p.scanpathMap.containsKey(pipeLineConstants.scan_path,p.scanpathMap)
-	//println p.scan_path
+	// Validating key in pipeline constants
+        p.scan_path = p.scanpathMap.containsKey(pipeLineConstants.scan_path,p.scanpathMap)
+	println p.scan_path
 	// configuring environment
         def  String env = p.parameterMap.get(pipeLineConstants.ENVIRONMENT)
         def line = env.substring(0, env.indexOf("-"))
